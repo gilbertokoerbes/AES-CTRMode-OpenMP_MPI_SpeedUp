f88f026aa7a17512d8caa2c3fe11bcaa5d37d30f
cp03 <cp03@lad.pucrs.br>
2024-06-06 23:17:29 -0300

    código funcional

--- aes_encrypt_ctr_mpi_openmp.c
+++ aes_encrypt_ctr_mpi_openmp.c
@@ -10,15 +10,26 @@
 #include <omp.h>
 #include <time.h>
 #include "mpi.h"
+#include "aes.h"
 
 #define AES128 1
 #define AES_BLOCK_SIZE 16
+#define MESSAGE_BLOCKS 4 //Blocos por mensagem
 #define ECB 1
 #define CBC 1
 #define CTR 1
+#define TAG_INIT_SLAVE 1
+#define TAG_SLAVE_REQ_WORK 2
+#define TAG_MASTER_SEND_WORK 3
+#define TAG_SLAVE_RESP_FINAL_WORK 4
+#define TAG_FINALIZE 6
 #define DEBUG 1
 
-#include "aes.h"
+typedef struct
+{
+    int block_id;
+    char message[AES_BLOCK_SIZE]; // Ponteiro para a string
+} CustomMessage;
 
 // Função para gerar o próximo bloco de CTR
 void generate_ctr_block(unsigned char *nonce, unsigned int counter, unsigned char *output)
@@ -36,34 +47,13 @@ void generate_ctr_block(unsigned char *nonce, unsigned int counter, unsigned cha
 int main(int argc, char *argv[])
 {
 
-    if(argc < 3){
-        printf("\n SCHEDULED and SET_NUM_THREADS_ARGV required\n");
+    int my_rank; // Identificador deste processo
+    int proc_n;  // Numero de processos disparados pelo usuário na linha de comando (np)
+    // char message[AES_BLOCK_SIZE]; // Buffer para as mensagens
+    CustomMessage msg;
+    int message_jumbo_blocks;
+    message_jumbo_blocks = 1;
 
-
-        return 0;
-    }
-
-    char* SCHEDULE_TYPE = argv[1];
-    int SET_NUM_THREADS_ARGV = atoi(argv[2]);
-    int chunk_size = 1;
-
-    omp_sched_t omp_schedule;
-    if (strcmp(SCHEDULE_TYPE, "static") == 0) {
-        omp_schedule = omp_sched_static;
-    } else if (strcmp(SCHEDULE_TYPE, "dynamic") == 0) {
-        omp_schedule = omp_sched_dynamic;
-    } else if (strcmp(SCHEDULE_TYPE, "guided") == 0) {
-        omp_schedule = omp_sched_guided;
-    } else if (strcmp(SCHEDULE_TYPE, "auto") == 0) {
-        omp_schedule = omp_sched_auto;
-    } else {
-        printf("Invalid schedule type\n");
-        return 1;
-    }
-
-    int my_rank;                  // Identificador deste processo
-    int proc_n;                   // Numero de processos disparados pelo usuário na linha de comando (np)
-    char message[AES_BLOCK_SIZE]; // Buffer para as mensagens
 
     MPI_Status status;
 
@@ -76,34 +66,67 @@ int main(int argc, char *argv[])
     // Pega informação do numero de processos (quantidade total)
     MPI_Comm_size(MPI_COMM_WORLD, &proc_n);
 
+    // int AES_BLOCK_SIZE = message_jumbo_blocks * AES_BLOCK_SIZE;
+    // // Alocação dinâmica para a mensagem
+    // msg.message = (char *)malloc((AES_BLOCK_SIZE*2) * sizeof(char));
+    // if (msg.message == NULL)
+    // {
+    //     fprintf(stderr, "Erro ao alocar memória msg.message\n");
+    //     MPI_Abort(MPI_COMM_WORLD, 1);
+    // }
+    // printf("size msg.message %lu\n", sizeof(msg.message));
+    // fflush(stdout);
+
+    
+    // Definindo o tipo MPI para CustomMessage
+    const int nitems = 2;
+    int blocklengths[2] = {1, AES_BLOCK_SIZE};
+    MPI_Datatype types[2] = {MPI_INT, MPI_CHAR};
+    MPI_Datatype MPI_CUSTOM_MESSAGE_TYPE;
+    MPI_Aint offsets[2];
+
+    offsets[0] = offsetof(CustomMessage, block_id);
+    offsets[1] = offsetof(CustomMessage, message);
+
+    MPI_Type_create_struct(nitems, blocklengths, offsets, types, &MPI_CUSTOM_MESSAGE_TYPE);
+    MPI_Type_commit(&MPI_CUSTOM_MESSAGE_TYPE);
+
     if (my_rank != 0) // SLAVE
     {
+        char hostname[1024];
+        hostname[1023] = '\0';
+        gethostname(hostname, 1023);
+        printf("Hostpedeira Slave %d: %s\n", my_rank, hostname);
+        fflush(stdout);
+        if (DEBUG)
+            printf("Processo Slave %d inicializando\n", my_rank);
+        fflush(stdout);
+
+        // MPI_Send(&request, 0);    // Solicita trabalho ao master
+        MPI_Send(&msg, 16, MPI_CUSTOM_MESSAGE_TYPE, 0, TAG_INIT_SLAVE, MPI_COMM_WORLD);
+
         while (1)
         {
-            // papel do escravo
-            if (DEBUG) printf("Processo Slave %d solicita trabalho\n", my_rank);
-            fflush(stdout);
 
-            // MPI_Send(&request, 0);    // Solicita trabalho ao master
-            MPI_Send(&message, 16, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
-
-            if (DEBUG) printf("Processo Slave %d esperando trabalho\n", my_rank);
+            if (DEBUG)
+                printf("Processo Slave %d esperando trabalho\n", my_rank);
             fflush(stdout);
 
             // MPI_Recv(&response, 0);    // recebo do mestre
-            MPI_Recv(&message, 16, MPI_CHAR, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-            if (DEBUG) printf("Processo Slave %d trabalho recebido\n", my_rank);
+            MPI_Recv(&msg, 16, MPI_CUSTOM_MESSAGE_TYPE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            if (DEBUG)
+                printf("Processo Slave %d trabalho recebido\n", my_rank);
             fflush(stdout);
 
-            if (status.MPI_TAG == 1) {
+            if (status.MPI_TAG == TAG_FINALIZE)
+            {
                 printf("Processo Slave %d BREAK!\n", my_rank);
-                fflush(stdout);                
+                fflush(stdout);
                 break;
             }
-                            
 
             char buffer_in[AES_BLOCK_SIZE];
-            memcpy(buffer_in, message, AES_BLOCK_SIZE);
+            memcpy(buffer_in, msg.message, AES_BLOCK_SIZE);
 
             int i = 0; //<<<<<<<<<<<<<
 
@@ -150,7 +173,7 @@ int main(int argc, char *argv[])
                 printf("\n");
                 fflush(stdout);
             }
-            
+
             unsigned char encrypted_output[AES_BLOCK_SIZE];
 
             // XOR entre o bloco criptografado do contador e o bloco de entrada
@@ -158,7 +181,7 @@ int main(int argc, char *argv[])
             for (int j = 0; j < AES_BLOCK_SIZE; ++j)
             {
                 // encrypted_output[i * AES_BLOCK_SIZE + j] = encrypted_counter_block[j] ^ in[i * AES_BLOCK_SIZE + j];
-                encrypted_output[i * AES_BLOCK_SIZE + j] = counter_block[j] ^ buffer_in[j];
+                encrypted_output[AES_BLOCK_SIZE + j] = counter_block[j] ^ buffer_in[j];
             }
             // fprintf(fptr_output, encrypted_output);
             fflush(stdout);
@@ -178,17 +201,26 @@ int main(int argc, char *argv[])
             // printf(".");
             fflush(stdout);
 
-            if (DEBUG) printf("Processo Slave %d respondendo trabalho\n", my_rank);
+            if (DEBUG)
+                printf("Processo Slave %d respondendo trabalho\n", my_rank);
             fflush(stdout);
 
-            memcpy(message, encrypted_output, AES_BLOCK_SIZE);
+            memcpy(msg.message, encrypted_output, AES_BLOCK_SIZE);
 
-            // MPI_Send(&message, 0);    // retorno resultado para o mestre
-            MPI_Send(&message, 16, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+            // MPI_Send(&msg, 0);    // retorno resultado para o mestre
+            MPI_Send(&msg, 16, MPI_CUSTOM_MESSAGE_TYPE, 0, 0, MPI_COMM_WORLD);
 
-            if (DEBUG) printf("Processo Slave %d Send concluído\n", my_rank);
+            if (DEBUG)
+                printf("Processo Slave %d Send concluído\n", my_rank);
             fflush(stdout);
-            
+
+            // papel do escravo
+            if (DEBUG)
+                printf("Processo Slave %d solicita trabalho\n", my_rank);
+            fflush(stdout);
+
+            // MPI_Send(&request, 0);    // Solicita trabalho ao master
+            MPI_Send(&msg, 16, MPI_CUSTOM_MESSAGE_TYPE, 0, TAG_SLAVE_REQ_WORK, MPI_COMM_WORLD);
         }
     }
 
@@ -202,6 +234,13 @@ int main(int argc, char *argv[])
         /////////////////////////////////////////////////////////////////
         /////////////////////////////////////////////////////////////////
         /////////////////////////////////////////////////////////////////
+        double t1,t2,tf;
+        t1 = MPI_Wtime(); 
+
+        char hostname[1024];
+        hostname[1023] = '\0';
+        gethostname(hostname, 1023);
+        printf("Hostpedeira Master: %s\n", hostname);
         FILE *arquivo;
         FILE *fptr_output;
         FILE *fptr_executions;
@@ -233,11 +272,22 @@ int main(int argc, char *argv[])
             printf("num_blocks %d", num_blocks);
         fflush(stdout);
 
-        // Criando buffer_in para armazenar a saída
+        // Criando buffer_input para armazenar a leitura do arquivo
+        unsigned char *buffer_input_file = (unsigned char *)malloc(AES_BLOCK_SIZE * num_blocks);
+        if (!buffer_input_file)
+        {
+            printf("Erro ao alocar memória. buffer_input_file\n");
+            fflush(stdout);
+            return 1;
+        }
+
+        fread(buffer_input_file, AES_BLOCK_SIZE, num_blocks, arquivo);
+
+        // Criando buffer_encrypted_output para armazenar a saída
         unsigned char *buffer_encrypted_output = (unsigned char *)malloc(AES_BLOCK_SIZE * num_blocks);
         if (!buffer_encrypted_output)
         {
-            printf("Erro ao alocar memória.\n");
+            printf("Erro ao alocar memória.buffer_encrypted_output \n");
             fflush(stdout);
 
             return 1;
@@ -271,84 +321,100 @@ int main(int argc, char *argv[])
             printf("\n");
             fflush(stdout);
         }
-        // Criandobuffer_in para armazenar a saída
-        // unsigned char *buffer_encrypted_output = (unsigned char *)malloc(num_blocks * AES_BLOCK_SIZE);
 
-        struct timespec start, end;
-        double time_spent;
-        // clock_gettime(CLOCK_MONOTONIC, &start);
-
-        omp_set_num_threads(SET_NUM_THREADS_ARGV);
-        omp_set_schedule(omp_schedule,chunk_size);
-        // Criptografando cada bloco de entrada usando CTR
-        #pragma omp parallel for schedule(runtime)
-        for (int i = 0; i < num_blocks; ++i)
+        int blk_pointer = 0;
+        while (1)
         {
 
-            fread(buffer_in, 1, AES_BLOCK_SIZE, arquivo);
+            if (blk_pointer >= num_blocks)
+            {
+                // SHUTDOWN...
+                for (int i = 1; i < proc_n; i++)
+                {
+                    printf("Shutdown proc_n: %d \n", i);
+                    fflush(stdout);
+                    MPI_Send(&msg, 16, MPI_CUSTOM_MESSAGE_TYPE, i, TAG_FINALIZE, MPI_COMM_WORLD);
+                }
+                break;
+            }
+
             if (DEBUG)
+                printf("Mestre aguardando requisicao\n");
+            fflush(stdout);
+
+            MPI_Recv(&msg, 16, MPI_CUSTOM_MESSAGE_TYPE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+            if (DEBUG)
+                printf("Mestre recebeu requisicao %d / tag :%d\n", status.MPI_SOURCE, status.MPI_TAG);
+            fflush(stdout);
+
+            int recv_tag = status.MPI_TAG;
+            if (recv_tag == TAG_INIT_SLAVE || recv_tag == TAG_SLAVE_REQ_WORK)
             {
-                int input_length = sizeof(buffer_in);
-                printf("\n in  em hex\n");
+
+                // fread(buffer_in, 1, AES_BLOCK_SIZE, arquivo); lemos todo arquivo em buffer_input
+                memcpy(buffer_in, buffer_input_file + blk_pointer, AES_BLOCK_SIZE);
+
+                if (DEBUG)
+                {
+                    int input_length = sizeof(buffer_in);
+                    printf("\n buffer_in  em hex\n");
+                    fflush(stdout);
+
+                    for (int i = 0; i < input_length; i++)
+                        printf("%02hhX", buffer_in[i]);
+                    fflush(stdout);
+                }
+                printf("recv_tag\n");
+                memcpy(msg.message, buffer_in, AES_BLOCK_SIZE);
+                msg.block_id = blk_pointer;
+
+                printf("recv_tag");
                 fflush(stdout);
 
-                for (int i = 0; i < input_length; i++)
-                    printf("%02hhX", buffer_in[i]);
+                // MPI_Send(&msg, status.MPI_SOURCE);
+                MPI_Send(&msg, 16, MPI_CUSTOM_MESSAGE_TYPE, status.MPI_SOURCE, TAG_MASTER_SEND_WORK, MPI_COMM_WORLD);
+                if (DEBUG)
+                    printf("Mestre enviou trabalho\n");
                 fflush(stdout);
             }
-
-            // recebo mensagens de qualquer emissor e com qualquer etiqueta (TAG)
-            if (DEBUG) printf("Mestre aguardando requisicao\n");
-            fflush(stdout);
-
-            // MPI_Recv(&message, MPI_ANY_SOURCE, MPI_ANY_TAG, status);  // recebo por ordem de chegada com any_source
-            MPI_Recv(&message, 16, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-            if (DEBUG) printf("Mestre recebeu requisicao %d\n", status.MPI_SOURCE);
-            fflush(stdout);
-
-            memcpy(message, buffer_in, AES_BLOCK_SIZE);
-
-            // MPI_Send(&message, status.MPI_SOURCE);
-            MPI_Send(&message, 16, MPI_CHAR, status.MPI_SOURCE, 0, MPI_COMM_WORLD);
-            if (DEBUG) printf("Mestre enviou trabalho\n");
-            fflush(stdout);
-
-            MPI_Recv(&message, 16, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-
-            memcpy(buffer_in, message, AES_BLOCK_SIZE);
-
-            for (int j = 0; j < AES_BLOCK_SIZE; ++j)
+            else if (recv_tag == TAG_SLAVE_RESP_FINAL_WORK)
             {
-                // buffer_encrypted_output[i * AES_BLOCK_SIZE + j] = encrypted_counter_block[j] ^ in[i * AES_BLOCK_SIZE + j];
-                buffer_encrypted_output[i * AES_BLOCK_SIZE + j] = buffer_in[j];
+
+                MPI_Recv(&msg, 16, MPI_CUSTOM_MESSAGE_TYPE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+                if (DEBUG)
+                    printf("Mestre recebeu trabalho processado %d\n", status.MPI_SOURCE);
+                fflush(stdout);
+
+                memcpy(buffer_in, msg.message, AES_BLOCK_SIZE);
+
+                int blk_id = msg.block_id;
+                for (int j = 0; j < AES_BLOCK_SIZE; ++j)
+                {
+                    // buffer_encrypted_output[i * AES_BLOCK_SIZE + j] = encrypted_counter_block[j] ^ in[i * AES_BLOCK_SIZE + j];
+                    buffer_encrypted_output[blk_id * AES_BLOCK_SIZE + j] = buffer_in[j];
+                }
+                // fprintf(fptr_output, buffer_encrypted_output);
+                fflush(stdout);
             }
-            // fprintf(fptr_output, buffer_encrypted_output);
-            fflush(stdout);
+            blk_pointer++;
         }
-
-        // SHUTDOWN...
-        for (int i = 1; i < proc_n; i++)
-        {   
-             printf("Shutdown proc_n: %d \n",i);
-            fflush(stdout);
-            MPI_Send(&message, 16, MPI_CHAR, i, 1, MPI_COMM_WORLD); 
-        }
-        
-
+        t2 = MPI_Wtime(); // termina a contagem do tempo
+        tf = t2-t1;
+        printf("\nTempo de execucao: %f\n\n", tf); 
         // // Obter o tempo final
         // clock_gettime(CLOCK_MONOTONIC, &end);
         // // Calcular o tempo decorrido
         // time_spent = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
 
         // for(int i =0; i<sizeof(buffer_encrypted_output); i++)
-        // fwrite(buffer_encrypted_output, sizeof(unsigned char), AES_BLOCK_SIZE, fptr_output);
+        //fwrite(buffer_encrypted_output, sizeof(unsigned char), AES_BLOCK_SIZE, fptr_output);
 
-        //fwrite(buffer_encrypted_output, AES_BLOCK_SIZE, AES_BLOCK_SIZE * num_blocks, fptr_output);
+        fwrite(buffer_encrypted_output, AES_BLOCK_SIZE, AES_BLOCK_SIZE * num_blocks, fptr_output);
 
-        //fprintf(fptr_executions, "%d;%f\n", num_blocks, time_spent);
+        fprintf(fptr_executions, "%d;%f\n", num_blocks, tf);
         fflush(stdout);
 
-        printf("\nTempo de execução: %f segundos\n", time_spent);
         fflush(stdout);
     }
 
